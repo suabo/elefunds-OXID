9fbea84ce64b0942ace78421c841e8a5c8e9f6ce
suabo <info@suabo.de>
2013-11-17 15:37:23 +0100

    Fixed Problem with scheduled donations. expand db field for elefunds state from 16 to 32 chars.

--- copy_this/modules/suabo/lfnds/setup/1.0.0/suabolfnds.sql
+++ copy_this/modules/suabo/lfnds/setup/1.0.0/suabolfnds.sql
@@ -2,7 +2,7 @@ CREATE TABLE IF NOT EXISTS `suabolfnds` (
   `oxid` varchar(32) COLLATE latin1_general_ci NOT NULL,
   `oxorderid` varchar(32) COLLATE latin1_general_ci NOT NULL,
   `lfndsdonation` longtext COLLATE latin1_general_ci NOT NULL,
-  `lfndsstate` varchar(16) CHARACTER SET utf8 NOT NULL,
+  `lfndsstate` varchar(32) CHARACTER SET utf8 NOT NULL,
   `lfndstime` datetime NOT NULL,
   UNIQUE KEY `oxid` (`oxid`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci;
